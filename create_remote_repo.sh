2120b7d752ad125855515154428af584f9769e52
root <derek_finlay@gmail.com>
2016-11-27 02:40:52 +0000

    Remove the files that were created by editor and added all current test scripts to the project

---
+++ create_remote_repo.sh
@@ -0,0 +1,3 @@
+curl -u 'derekfinlay' https://api.github.com/derekfinlay/repos -d '{"name":"edison","description":"Creating and testing python code on the intel edison"}'
+#git remote add origin git@github.com:derekfinlay/projectname.git
+#git push origin master
